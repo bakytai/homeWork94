ce9f4db498751d2b885f70809cd60305ed003d32
Bakytai Egemberdieva <bakijw@gmail.com>
2022-04-05 18:54:59 +0600

    подключила модули

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumController } from './album/album.controller';
 import { TrackController } from './track/track.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Artist, ArtistSchema } from './schemas/artist.schema';
+import { Album, AlbumSchema } from './schemas/album.schema';
+import { Track, TrackSchema } from './schemas/track.schema';
+import { ArtistController } from './artist/artist.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, AlbumController, TrackController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/musicApp'),
+    MongooseModule.forFeature([
+      { name: Artist.name, schema: ArtistSchema },
+      { name: Album.name, schema: AlbumSchema },
+      { name: Track.name, schema: TrackSchema },
+    ]),
+  ],
+  controllers: [
+    AppController,
+    AlbumController,
+    TrackController,
+    ArtistController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
